1e829e7b1ec2b1a8560ff094c409db0e4fa6e0ce
Hoai Anh <hoaianh16022005@gmail.com>
2025-10-23 21:41:52 +0700

    Nhúng trang chủ (đang nhập) người dùng

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/homepage-login-user', function () {
+    return view('homepage-login-user');
+});
